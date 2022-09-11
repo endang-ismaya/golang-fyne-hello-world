50206310fc5abae044f4e3af4c978411c4a03488
Endang Ismaya <endang.ismaya@gmail.com>
2022-09-11 13:22:08 +0700

    add btn.Importance

--- main.go
+++ main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
-func main() {
-	a := app.New()
+type App struct {
+	output *widget.Label
+}
 
-	mainWindow := a.NewWindow("Hello World")
+var myApp App
+
+func main() {
+	a := app.New()                           // one application should have and app.New()
+	mainWindow := a.NewWindow("Hello World") // at least has one window
+
+	// make UI
+	output, entry, btn := myApp.makeUI()
 
 	// put some content inside main_window
-	mainWindow.SetContent(widget.NewLabel("Hello World"))
-	mainWindow.ShowAndRun()
+	// container vbox
+	mainWindow.SetContent(container.NewVBox(output, entry, btn))
+	mainWindow.Resize(fyne.Size{Width: 500, Height: 500})
+	mainWindow.ShowAndRun() // run an event loop (listen for events)
+}
+
+func (app *App) makeUI() (*widget.Label, *widget.Entry, *widget.Button) {
+	output := widget.NewLabel("Hello, World!")
+	entry := widget.NewEntry()
+	btn := widget.NewButton("Enter", func() {
+		app.output.SetText(entry.Text)
+	})
+
+	btn.Importance = widget.HighImportance
+
+	app.output = output
+	return output, entry, btn
 }
