b4a3ccb99450baa8595640813c665becd6b0e6ca
Endang Ismaya <endang.ismaya@gmail.com>
2022-09-11 13:06:43 +0700

    add first HelloWorld App

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
 
+func main() {
+	a := app.New()
+
+	mainWindow := a.NewWindow("Hello World")
+
+	// put some content inside main_window
+	mainWindow.SetContent(widget.NewLabel("Hello World"))
+	mainWindow.ShowAndRun()
 }
